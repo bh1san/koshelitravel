f68ee36b461a93fe6cabd8b217c681682818e439
Chhatradev Chaudhary <bhisan25@gmail.com>
2025-06-26 22:17:12 +0000

    0|kosheli-travel  |  ⚠ Compiled with warnings 0|kosheli-travel  | ./node

---
+++ src/next.config.js
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: [
+      '@opentelemetry/instrumentation',
+      'handlebars',
+      '@opentelemetry/sdk-node',
+      '@opentelemetry/exporter-jaeger'
+    ],
+  },
+};
+
+module.exports = nextConfig;
