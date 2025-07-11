64e203abf37f44942972ede57e3f1f3235c515eb
Chhatradev Chaudhary <bhisan25@gmail.com>
2025-07-11 17:15:46 +0000

    still nothing changed. can we try new way

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 
 import type {Metadata} from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
+import './globals.css?v=2';
 import { Toaster } from '@/components/ui/toaster';
 import Script from 'next/script';
 
