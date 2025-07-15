0e50e197b48af4bf414386832830af679778e8de
Chhatradev Chaudhary <bhisan25@gmail.com>
2025-07-15 00:03:47 +0000

    still same

--- next.config.ts
+++
@@ -1,1 +0,0 @@
-// This file is unused and caused a potential configuration conflict. It is now empty.
--- src/next.config.js
+++
@@ -1,2 +0,0 @@
-// This file is not used. The Next.js configuration is located in the root next.config.js file.
-// This file can be safely deleted.
