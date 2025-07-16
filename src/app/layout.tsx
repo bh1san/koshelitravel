192f18a2aff6974ac17aaa46a6aa15152740cacf
Chhatradev Chaudhary <bhisan25@gmail.com>
2025-07-16 20:26:20 +0000

    add this code between the <head></head> tags on each page of the site.:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,8 @@ export default function RootLayout({
   return (
     <html lang="en">
        <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3785928841766736"
+     crossorigin="anonymous"></script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
