1cf33b62653b0256e9c44204dcfb20bd7237879e
Chhatradev Chaudhary <bhisan25@gmail.com>
2025-07-24 09:31:39 +0000

    why still its showing same error.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,13 +52,6 @@ export default {
         input: 'hsl(var(--input))',
         'input-border': 'hsl(var(--input-border))',
         ring: 'hsl(var(--ring))',
-        chart: {
-          '1': 'hsl(var(--chart-1))',
-          '2': 'hsl(var(--chart-2))',
-          '3': 'hsl(var(--chart-3))',
-          '4': 'hsl(var(--chart-4))',
-          '5': 'hsl(var(--chart-5))',
-        },
       },
       borderRadius: {
         lg: 'var(--radius)',
