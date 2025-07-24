66579d03fb5766df05aa37c3f1fa24aa8f5c6f29
Chhatradev Chaudhary <bhisan25@gmail.com>
2025-07-24 20:27:19 +0000

    root@Koshelitravel:~/Koshelitravel# npm install
    added 1 package, change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 
 import type {Metadata} from 'next';
-import './globals.css';
+import '@/app/globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { WhatsappChat } from '@/components/common/whatsapp-chat';
 import { Inter } from 'next/font/google';
