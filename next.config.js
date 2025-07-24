705ceca0d39d01cb0760e97cc83c11ece4e713d7
Chhatradev Chaudhary <bhisan25@gmail.com>
2025-07-24 09:42:31 +0000

    why still not showing the page preview.

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'www.imghippo.com',
+        hostname: 'i.imghippo.com',
         port: '',
         pathname: '/**',
       },
